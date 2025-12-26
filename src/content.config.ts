91e48ac1c8df732ee88976c7bdd0928f1e41b81f
Michal Pasierbski <mpasierbski@gmail.com>
2025-12-26 13:15:49 +0100

    Add projects content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,5 +11,16 @@ const blog = defineCollection({
         image: z.string().optional(),
     }),
 });
+const projects = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/projects" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        status: z.string(),
+        tags: z.array(z.string()),
+        url: z.string(),
+        image: z.string(),
+    }),
+});
 
-export const collections = { blog };
+export const collections = { blog, projects };
