4501ebc5830f334d6e081f5efb5f3028fd26b116
Michal Pasierbski <mpasierbski@gmail.com>
2025-12-26 17:31:41 +0100

    Fix project image

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,23 +3,23 @@ import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
     loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
         pubDate: z.coerce.date(),
         author: z.string(),
-        image: z.string().optional(),
+        image: image().optional(),
     }),
 });
 const projects = defineCollection({
     loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/projects" }),
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
         status: z.string(),
         tags: z.array(z.string()),
         url: z.string(),
-        image: z.string(),
+        image: image(),
     }),
 });
 
